4ee516d88fc4b997fefeb3969e0c9520d9c4cd51
Michael Mueller <michael@miniguide.co>
2020-05-15 18:01:10 +0200

    add user update route

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
 
-const { checkUsernameAndPasswordNotEmpty } = require('../middlewares');
+const { checkUsernameNotEmpty, checkUsernameAndPasswordNotEmpty } = require('../middlewares');
 
 const User = require('../models/User');
 
@@ -23,6 +23,30 @@ router.post('/', checkUsernameAndPasswordNotEmpty, async (req, res, next) => {
 		const newUser = await User.create({ username, hashed_password });
 		req.session.currentUser = newUser;
 		return res.status(201).json(newUser);
+	} catch (error) {
+		next(error);
+	}
+});
+
+
+router.get('/:id', async (req, res, next) => {
+	const { id } = req.params;
+	try {
+		const user = await User.findById(id);
+		return res.json({ code: 'user-read', user });
+	} catch (error) {
+		next(error);
+	}
+});
+
+router.put('/:id', checkUsernameNotEmpty, async (req, res, next) => {
+	const { id } = req.params;
+	const { user } = res.locals;
+	console.log('id is', id);
+	console.log('received user data', user);
+	try {
+		await User.findByIdAndUpdate(id, user);
+		return res.status(201).json({ code: 'user-updated', user });
 	} catch (error) {
 		next(error);
 	}

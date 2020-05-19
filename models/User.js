0af15cc93a6844cb214d5ba519be5f2b987fa7de
Michael Mueller <michael@miniguide.co>
2020-05-19 21:12:57 +0200

    add default user profile image

--- models/User.js
+++ models/User.js
@@ -6,7 +6,7 @@ const userSchema = new Schema(
 	{
 		username: { type: String, required: true, unique: true, trim: true },
 		hashed_password: { type: String, required: true },
-		image: { type: String },
+		image: { type: String, default: 'https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png' },
 		bio: { type: String },
 		url: { type: String },
 		points: { type: Number, default: 0 },

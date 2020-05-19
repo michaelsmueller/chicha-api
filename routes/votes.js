c31fe48a5c184dca3c0ef8aea37ab4cdf85886d6
Michael Mueller <michael@miniguide.co>
2020-05-19 18:48:18 +0200

    add/remove points to user whose event was voted

--- routes/votes.js
+++ routes/votes.js
@@ -11,13 +11,15 @@ router.post('/', async (req, res, next) => {
 	const { eventId, direction } = req.body;
 	const { currentUser: { _id: userId } } = req.session;
 	try {
-		const newVote = await Vote.create({
-			voter: ObjectID(userId),
-			event: ObjectID(eventId),
-			direction,
-		});
+		const newVote = await Vote.create({ voter: ObjectID(userId), event: ObjectID(eventId), direction });
+
 		await Event.findByIdAndUpdate(eventId, { $inc: { votes: direction } });
 		await User.findByIdAndUpdate(userId, { $inc: { points: 1 } });
+
+		const { creator: creatorId } = await Event.findById(eventId).select({ _id: 0, creator: 1 });
+		const creator = await User.findById(creatorId);
+		if (creator) await User.findByIdAndUpdate(creatorId, { $inc: { points: direction } });
+
 		return res.status(200).json({ code: 'vote-created', newVote });
 	} catch (error) {
 		next(error);
@@ -42,6 +44,11 @@ router.put('/:id', async (req, res, next) => {
 	try {
 		await Vote.findByIdAndUpdate(id, { direction });
 		await Event.findByIdAndUpdate(eventId, { $inc: { votes: 2 * direction } });
+
+		const { creator: creatorId } = await Event.findById(eventId).select({ _id: 0, creator: 1 });
+		const creator = await User.findById(creatorId);
+		if (creator) await User.findByIdAndUpdate(creatorId, { $inc: { points: 2 * direction } });
+
 		return res.status(201).json({ code: 'vote-changed', direction });
 	} catch (error) {
 		next(error);
@@ -53,10 +60,16 @@ router.delete('/:id', async (req, res, next) => {
 	const { id } = req.params;
 	const { eventid: eventId, direction } = req.query;
 	const { currentUser: { _id: userId } } = req.session;
-	await Event.findByIdAndUpdate(eventId, { $inc: { votes: direction } });
-	await User.findByIdAndUpdate(userId, { $inc: { points: -1 } });
 	try {
 		await Vote.findByIdAndDelete(id);
+
+		await Event.findByIdAndUpdate(eventId, { $inc: { votes: direction } });
+		await User.findByIdAndUpdate(userId, { $inc: { points: -1 } });
+
+		const { creator: creatorId } = await Event.findById(eventId).select({ _id: 0, creator: 1 });
+		const creator = await User.findById(creatorId);
+		if (creator) await User.findByIdAndUpdate(creatorId, { $inc: { points: direction } });
+
 		return res.status(200).json({ code: 'vote-removed', id });
 	} catch (error) {
 		next(error);

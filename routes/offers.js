b91777ea3420c919c3f0389e5763d70947abe0c2
Michael Mueller <michael@miniguide.co>
2020-05-21 23:24:22 +0200

    add offerRouter

---
+++ routes/offers.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const Offer = require('../models/Offer');
+
+const router = express.Router();
+
+router.get('/', async (req, res, next) => {
+	try {
+		const offers = await Offer.find().sort('cost');
+		return res.json({ code: 'offers-read', offers });
+	} catch (error) {
+		next(error);
+	}
+});
+
+router.get('/:id', async (req, res, next) => {
+	const { id } = req.params;
+	try {
+		const offer = await Offer.findById(id);
+		return res.json({ code: 'offer-read', offer });
+	} catch (error) {
+		next(error);
+	}
+});
+
+module.exports = router;
